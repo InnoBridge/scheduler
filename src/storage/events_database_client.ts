f97a6a89a557131bf01b06192080be386313bdf7
Yi leng Yao <yilengyao@gmail.com>
2025-07-14 19:45:36 -0700

    add getEventById

--- src/storage/events_database_client.ts
+++ src/storage/events_database_client.ts
@@ -2,9 +2,10 @@ import { BaseDatabaseClient } from '@/storage/base_database_client';
 import { Event, EventStatus } from '@/models/events';
 
 interface EventsDatabaseClient extends BaseDatabaseClient {
+    getEventById(eventId: string): Promise<Event | null>;
     getEventsByProvider(providerId: string): Promise<Event[]>;
     getEventsByCustomer(customerId: string): Promise<Event[]>;
-    getEventByProviderOrCustomer(userId: string): Promise<Event[]>;
+    getEventsByProviderOrCustomer(userId: string): Promise<Event[]>;
     createEvent(event: Event): Promise<Event>;
     updateEventStatus(eventId: string, status: EventStatus, customerId?: string, color?: string): Promise<Event>;
     deleteEvent(eventId: string): Promise<void>;
